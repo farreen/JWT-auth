6ed9467c06b86ac7ddf8e9f652d736567bc7cea2
Farah Nasreen <farahnasreen2000@gmail.com>
2023-08-13 00:23:25 +0530

    Error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,25 @@
+import { UserModel } from "../models/user.js";
+
+const handleError = (err) => {
+    console.log("err....", err.code)
+    let errors = { email: "", username: "", password: "" }
+
+    // duplicate error code
+    if(err.code === 11000){
+        errors.email = 'that email is already registered';
+        return errors
+    }
+
+    // validation error
+    if (err.message.includes("User validation failed")) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}
+
+
 // SIGN UP PAGE 
 export const signup_get = (req, res) => {
     res.send("signup_get")
@@ -8,8 +30,17 @@ export const login_get = (req, res) => {
 }
 
 // CREATE A NEW USER IN THE DATABASE
-export const signup_post = (req, res) => {
-    res.send("New Signup")
+export const signup_post = async (req, res) => {
+    console.log("req_data", req.body)
+    const { email, username, password } = req.body
+    try {
+        const saved_data = await UserModel.create({ email, username, password })
+        console.log("saved", saved_data)
+        res.status(200).send(`New Signup${saved_data}`)
+    } catch (err) {
+        const error = handleError(err);
+        res.status(500).json(error);
+    }
 }
 
 // AUTHENTICATE A CURRENT USER
