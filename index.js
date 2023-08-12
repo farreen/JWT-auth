87ddb792fa26340687b35b75b85c43d30a225d74
Farah Nasreen <farahnasreen2000@gmail.com>
2023-08-13 00:16:03 +0530

    Import authRoutes from routes folder and pass it to app.use middleware

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import connection from './database/db.js';
+import authRoutes from './routes/authRoutes.js';
 const PORT = "8080";
 const app = express();
+app.use(express.json())
+
+app.use(authRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running at port 8080`)
