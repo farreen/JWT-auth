ae3e4dfa556353daff4f00ff86dac1d51b5c39a5
Farah Nasreen <farahnasreen2000@gmail.com>
2023-08-16 16:45:41 +0530

    Comment unnecessary code

--- index.js
+++ index.js
@@ -10,19 +10,18 @@ app.use(cookieParser());
 app.use(authRoutes)
 
 // COOKIE
+// app.get("/set-cookies", (req, res) => {
+//     res.cookie("newCookie", false);
+//     res.cookie("isEmployee", true, {maxAge: 1000*60*60*24, httpOnly: true})
+//     res.send("you set the cookie");
+// })
 
-app.get("/set-cookies", (req, res) => {
-    res.cookie("newCookie", false);
-    res.cookie("isEmployee", true, {maxAge: 1000*60*60*24, httpOnly: true})
-    res.send("you set the cookie");
-})
 
-
-app.get("/get-cookies", (req, res) => {
-    const cookies = req. cookies;
-    console.log("cokkies", cookies);
-    res.json(cookies);
-})
+// app.get("/get-cookies", (req, res) => {
+//     const cookies = req. cookies;
+//     console.log("cokkies", cookies);
+//     res.json(cookies);
+// })
 
 app.listen(PORT, () => {
     console.log(`server is running at port 8080`)
