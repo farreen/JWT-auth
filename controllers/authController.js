88491fc9e3827e39ae1a40887f0041406c390555
Farah Nasreen <farahnasreen2000@gmail.com>
2023-08-17 18:53:54 +0530

    Create secret key and verify token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 import { UserModel } from "../models/user.js";
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+const secretKey = process.env.JWT_SECRET_KEY 
 const handleError = (err) => {
     console.log("err....", err.message)
     let errors = { email: "", username: "", password: "" }
@@ -32,7 +35,7 @@ const handleError = (err) => {
 // CREATE JWT TOKEN
 const maxAge = 60*60*24*5;
 const createToken = (id) => {
-    return jwt.sign({id}, "farah", {
+    return jwt.sign({id}, secretKey, {
         expiresIn: maxAge
     })
 }
@@ -55,7 +58,7 @@ export const signup_post = async (req, res) => {
         const saved_user = await UserModel.create({ email, username, password })
         console.log("saved", saved_user)
         const token = createToken(saved_user._id)
-        res.cookie("jwt-token", token, {httpOnly: true, maxAge: maxAge*1000})
+        res.cookie("jwtToken", token, {httpOnly: true, maxAge: maxAge*1000})
         res.status(201).json({user: saved_user._id})
     } catch (err) {
         const error = handleError(err);
@@ -69,7 +72,7 @@ export const login_post = async (req, res) => {
     try {
         const user = await UserModel.login(email, password);
         const token = createToken(user._id)
-        res.cookie("jwt-token", token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.cookie("jwtToken", token, { httpOnly: true, maxAge: maxAge * 1000 })
         res.status(200).json({ userLoggedin: user._id })
     } catch (error) {
         console.log("error", error)
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,12 @@
 import jwt from "jsonwebtoken"
-
+import dotenv from 'dotenv';
+dotenv.config();
+const secretKey = process.env.JWT_SECRET_KEY
 export const requireAuth = (req, res, next) => {
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwtToken;
     // check jwt-token exist and verified 
     if (token) {
-        jwt.verify(token, "farah", (err, decodedToken) => {
+        jwt.verify(token, secretKey, (err, decodedToken) => {
             if (err) {
                 // redirect user to login page
                 res.status(401).send("token not available");
